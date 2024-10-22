b41a4769dbbbb6568add231e285d5d16ed24480a
pattyyepez <150443257+pattyyepez@users.noreply.github.com>
2024-10-22 23:45:53 +0200

    database and java server1

---
+++ sep/src/test/java/dk/via/sep/SepApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.via.sep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
