e71b9b5390944df4ba65d96e71d0f705ff6c17c1
DominikVIA <144497193+DominikVIA@users.noreply.github.com>
2024-12-07 13:43:47 +0100

    🦨💨😤

--- Database/housePalDB.sql
+++ Database/housePalDB.sql
@@ -136,25 +136,26 @@ CREATE TABLE Application (
 );
 
 CREATE TABLE House_review (
-    id SERIAL PRIMARY KEY,
     profile_id INT NOT NULL,
     sitter_id INT NOT NULL,
+
     rating INT CHECK (rating BETWEEN 1 AND 5),
     comments VARCHAR(1000) NOT NULL,
     date TIMESTAMP NOT NULL,
     FOREIGN KEY (profile_id) REFERENCES HouseProfile(profile_id) ON DELETE CASCADE,
-    FOREIGN KEY (sitter_id) REFERENCES HouseSitter(sitter_id) ON DELETE CASCADE
+    FOREIGN KEY (sitter_id) REFERENCES HouseSitter(sitter_id) ON DELETE CASCADE,
+    PRIMARY KEY (profile_id, sitter_id)
 );
 
 CREATE TABLE Sitter_review (
-    id SERIAL PRIMARY KEY,
     owner_id INT NOT NULL,
     sitter_id INT NOT NULL,
     rating INT CHECK (rating BETWEEN 1 AND 5),
     comments VARCHAR(1000) NOT NULL,
     date TIMESTAMP NOT NULL,
     FOREIGN KEY (owner_id) REFERENCES HouseOwner(owner_id) ON DELETE CASCADE,
-    FOREIGN KEY (sitter_id) REFERENCES HouseSitter(sitter_id) ON DELETE CASCADE
+    FOREIGN KEY (sitter_id) REFERENCES HouseSitter(sitter_id) ON DELETE CASCADE,
+    PRIMARY KEY (owner_id, sitter_id)
 );
 
 CREATE TABLE Report (
@@ -279,15 +280,11 @@ INSERT INTO Application (listing_id, sitter_id, message, status, date) VALUES
 (1, 5, 'I am available and love taking care of pets!', 'Pending', '2024-10-22 '),
 (1, 6, 'Experienced sitter available for your house and garden.', 'Rejected', '2024-10-23');
 INSERT INTO House_review (profile_id, sitter_id, rating, comments, date) VALUES
-(3, 5, 5, 'Beautiful house, great experience!', '2024-10-20'),
-(2, 6, 4, 'Lovely home, perfect location.', '2024-10-21');
+(1, 5, 5, 'Beautiful house, great experience!', '2024-10-20'),
+(1, 6, 4, 'Lovely home, perfect location.', '2024-10-21');
 INSERT INTO Sitter_review (owner_id, sitter_id, rating, comments, date) VALUES
 (1, 5, 5, 'Excellent sitter, took great care of our pets.', '2024-10-22'),
-(1, 5, 3, 'Excellent sitter, took great care of our pets.', '2024-10-22'),
-(1, 5, 4, 'Excellent sitter, took great care of our pets.', '2024-10-22'),
-(1, 5, 3, 'Excellent sitter, took great care of our pets.', '2024-10-22'),
-(1, 5, 2, 'Excellent sitter, took great care of our pets.', '2024-10-22'),
-(2, 6, 4, 'Very reliable and responsible.', '2024-10-21');
+(1, 6, 4, 'Very reliable and responsible.', '2024-10-21');
 INSERT INTO Report (reporting_id, reported_id, admin_id, comments, status) VALUES
 -- (1, 5, 1, 'Sitter was not responsive.', 'Pending'),
 (2, 6, 2, 'House was left untidy.', 'Resolved');
